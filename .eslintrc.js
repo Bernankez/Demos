4b33356d442b5922309b8e27807953405b598b2d
cole <bernankez@qq.com>
2020-09-13 22:59:41 +0800

    config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,17 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'eslint:recommended',
+    '@vue/prettier'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-mixed-spaces-and-tabs': [0, 'smart-tabs'],
+    'semi': [1, 'always'],
+    'indent': [0, 4]
   }
 }
