87d3402615db0585864eee75ca7aef9fbc71961a
shengbeichen <shengbeichen@inspur.com>
2020-09-15 17:52:10 +0800

    card贴顶实现 待实现card不能重复点击 content拉伸填充

--- src/views/applelike-card/card.vue
+++ src/views/applelike-card/card.vue
@@ -1,165 +1,128 @@
 <template>
-  <div>
-    <div ref="card" id="card" :class="{ card: true, active: isExpand }" @click="onExpand()">
-      <img src="../../assets/image/photo-1589963348451-142110e1ad3f.jpg" />
-      <div class="title">APP OF THE DAY</div>
-      <div class="content-wrapper">
-        <div class="content">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris
-          </p>
-          <p>
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident,
-          </p>
-          <p>sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris
-          </p>
-          <p>
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident,
-          </p>
-          <p>sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris
-          </p>
-          <p>
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident,
-          </p>
-          <p>sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris
-          </p>
-          <p>
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident,
-          </p>
-          <p>sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div>
+        <div :class="{ card: true, active: isExpand }" :style="{'--data-offset-top': dataOffsetTop}" @click="onExpand($event)">
+            <img :src="card.img" />
+            <div class="title">{{card.title}}</div>
+            <div class="content-wrapper">
+                <div class="content" v-html="card.text">
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'card',
-  components: {},
-  data() {
-    return {
-      cardItem: null,
-      scrollTop: 0,
-      isExpand: false,
-    };
-  },
-  created() {},
-  mounted() {},
-  watch: {
-    data(oldVal, newVal) {
-      this.$nextTick(() => {
-        this.cardItem = this.$refs.card;
-        cardItem.addEventListener('scroll', this.getElementScrollTop);
-      });
+    name: 'card',
+    components: {},
+    props: {
+        card: {
+            type: Object,
+            default: null
+        }
     },
-  },
-  methods: {
-    onExpand() {
-      this.isExpand = !this.isExpand;
+    data() {
+        return {
+            dataOffsetTop: 0,
+            isExpand: false,
+        };
     },
-    getElementScrollTop(e) {
-      this.scrollTop = this.cardItem.scrollTop;
-      console.log(this.scrollTop);
+    created() { },
+    mounted() { },
+    watch: {
     },
-  },
+    methods: {
+        onExpand(event) {
+            let cardItem = event.currentTarget;
+            let scrollTop = cardItem.getBoundingClientRect().top;
+            this.dataOffsetTop = `${-1 * scrollTop}px`;
+            this.isExpand = !this.isExpand;
+        },
+    },
 };
 </script>
 
 <style lang="scss" scope>
 body {
-  --card-width: 94vw;
-  --card-height: 58vw;
-  --image-height: 45vw;
-  --image-height-expanded: 60vw;
+    --card-width: 94vw;
+    --card-height: 58vw;
+    --image-height: 45vw;
+    --image-height-expanded: 60vw;
+    --title-height: calc(var(--card-height) - var(--image-height));
 }
 .card {
-  display: flex;
-  flex-direction: column;
-  margin: 1rem 0;
-  width: var(--card-width);
-  height: var(--card-height);
-  background-color: #fff;
-  border-radius: 1rem;
-  box-shadow: 0 0.2rem 2rem rgba(0, 0, 0, 0.1);
-  transition: 0.3s all cubic-bezier(0, 0.8, 1, 1.4);
-  font-size: 15px;
-  font-family: Helvetica;
+    display: flex;
+    flex-direction: column;
+    margin: 1rem 0;
+    width: var(--card-width);
+    height: var(--card-height);
+    background-color: #fff;
+    border-radius: 1rem;
+    box-shadow: 0 0.2rem 2rem rgba(0, 0, 0, 0.1);
+    transition: 0.3s all cubic-bezier(0, 1, 1, 1);
+    font-size: 15px;
+    font-family: Helvetica;
 }
 
 .card img {
-  display: block;
-  width: 100%;
-  height: var(--image-height);
-  object-fit: cover;
+    display: block;
+    width: 100%;
+    height: var(--image-height);
+    object-fit: cover;
 
-  border-top-left-radius: 1rem;
-  border-top-right-radius: 1rem;
+    border-top-left-radius: 1rem;
+    border-top-right-radius: 1rem;
 }
 
 .card .title {
-  display: flex;
-  align-items: center;
-  flex-shrink: 0;
-  flex-grow: 0;
-  height: calc(var(--card-height) - var(--image-height));
-  padding: 0 1.2rem;
-  box-sizing: border-box;
-  font-size: 1.3rem;
-  font-weight: bold;
-  letter-spacing: -0.5px;
-  //   background-color: #fff;
+    display: flex;
+    align-items: center;
+    flex-shrink: 0;
+    flex-grow: 0;
+    height: var(--title-height);
+    padding: 0 1.2rem;
+    box-sizing: border-box;
+    font-size: 1.3rem;
+    font-weight: bold;
+    letter-spacing: -0.5px;
+    //   background-color: #fff;
 }
 
 .card .content-wrapper {
-  transform: translateY(-1px);
-  height: 0;
-  overflow: hidden;
+    transform: translateY(-1px);
+    height: 0;
+    overflow: hidden;
 }
 
 .card .content-wrapper .content {
-  padding: 0 1.2rem;
-  background-color: #fff;
-  overflow: auto;
+    padding: 0 1.2rem;
+    background-color: #fff;
+    overflow: auto;
 }
 
 .card p {
-  font-size: 1.1rem;
-  line-height: 1.5rem;
+    font-size: 1.1rem;
+    line-height: 1.5rem;
 }
 
 .card.active {
-  transform: translateY(-1rem) scale(calc(100 / 94));
-  transform-origin: 50% 0;
-  border-radius: 0;
+    transform: translateY(var(--data-offset-top)) scale(calc(100 / 94));
+    transform-origin: 50% 0;
+    border-radius: 0;
 }
 
 .card.active .title {
-  background-color: #fff;
+    background-color: #fff;
 }
 
 .card.active img {
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-  height: var(--image-height-expanded);
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+    height: var(--image-height-expanded);
 }
 
 .card.active .content-wrapper {
-  height: 100vh;
-  overflow: visible;
+    height: 100vh;
+    overflow: visible;
 }
 </style>
--- src/views/applelike-card/index.vue
+++ src/views/applelike-card/index.vue
@@ -1,42 +1,49 @@
 <template>
-  <div>
-    <div class="content">
-      <card></card>
-      <!-- <card></card>
-      <card></card> -->
+    <div>
+        <div class="content">
+            <card :card="cardInfo"></card>
+            <card :card="cardInfo"></card>
+            <card :card="cardInfo"></card>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import card from './card';
 
 export default {
-  name: 'index',
-  components: { card },
-  data() {
-    return {};
-  },
-  created() {},
-  mounted() {},
-  methods: {},
+    name: 'index',
+    components: { card },
+    data() {
+        return {
+            cardInfo: {
+                img: require('../../assets/image/photo-1589963348451-142110e1ad3f.jpg'),
+                title: 'APP OF THE DAY',
+                text: '<p>asdasdasd</p><p>asdasdasd</p>'
+            }
+        };
+    },
+    created() { },
+    mounted() { },
+    methods: {
+    },
 };
 </script>
 
 <style lang="scss" scope>
 body {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  overflow-x: hidden;
-  background-color: #eee;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 100vh;
-  padding: 1rem, 0;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow-x: hidden;
+    background-color: #eee;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-height: 100vh;
+    padding: 1rem, 0;
 }
 </style>
